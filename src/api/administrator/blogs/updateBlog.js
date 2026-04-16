a01305ca44a16d6622cf83c0614d5fd1183a6e6d
Aditya Sahgal <aditya@Adityas-Mac-mini.local>
2026-04-16 10:43:41 +0530

    feat(update blog): implement update blog API

--- src/api/administrator/blogs/updateBlog.js
+++ src/api/administrator/blogs/updateBlog.js
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import api from "../../../utils/axios";
 
 const updateBlog = async ({ id, data }) => {
-  const response = await api.put(`/admin/blogs/update/${id}`, data);
+  const response = await api.post(`/admin/blogs/update`, { id, ...data });
   return response.data;
 };
 
--- src/api/administrator/news/updateNews.js
+++ src/api/administrator/news/updateNews.js
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import api from "../../../utils/axios";
 
 const updateNews = async ({ id, data }) => {
-  const response = await api.put(`/admin/news/update/${id}`, data);
+  const response = await api.post(`/admin/news/update`, { id, ...data });
   return response.data;
 };
 
